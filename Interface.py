5ae444f6a1e229e4b10cb4f50357a6d4ee609253
Bemadji Arsene <57248473+Bemadji@users.noreply.github.com>
2023-01-17 16:51:25 +0100

    Update Interface.py
    j'ai juste complété le code et j'ai changé le title aussi

--- Interface.py
+++ Interface.py
@@ -2,8 +2,17 @@ from tkinter import *
 import tkinter as tkinter
 
 root = Tk()
-root.title("Connexion")
+root.title("Bienvenue sur la plateforme d'Orientation Universitaire !")
 root.geometry("950*550 +180+50")
+logo=Canvas(root,width=900,height=500,bg="blue")
+logo.place(x=10,y=7)
+log=Label(logo,text="Bienvenue sur la plateforme",font=("arial",19,"bold"))
+log.place(x=20,y=10)
+identifiant=Label(logo,text="Saisir le email ou le tel",bg="blue",font=("arial",15,"bold"),fg="#fff")
+identifiant.place(x=350,y=130)
+
+mdp=Button(logo,text="Mot de passe",bg="blue",font=("arial",15))
+mdp.place(x=400,y=190)
 
 root.mainloop()
 
